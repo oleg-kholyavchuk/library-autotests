e062a586c6a5e69aa85ee8a4c5f50bf75d4d8c01
Oleg <oleg.kholyavchuk@bk.ru>
2022-09-27 16:36:28 +0300

    Autotest

---
+++ src/main/java/ru/buttonone/LibraryAutotestsApplication.java
@@ -0,0 +1,13 @@
+package ru.buttonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutotestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAutotestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/buttonone/LibraryAutotestsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.buttonone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAutotestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
